59be97b1d9364abea02b5b76ad0904affbdb4594
syang69 <syang69@csc208.csudh.edu>
2020-01-29 17:10:18 -0800

    first version

---
+++ syang3.c
@@ -0,0 +1,48 @@
+/*
+Simon Yang
+CSC-251-80
+1/29/20
+Lab 3
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+
+{
+int x=0, y=0,z=0, total=0;
+char counta, countb, countc, check;
+/*
+while (check != 'z')
+	{
+	printf("enter a grade:");
+	scanf("%c", &check);
+
+	if(check == 'a')
+		{
+		counta++;
+		total++;
+		}
+	else if (check == 'b')
+		{
+		countb++;
+		total++;
+		}
+	}
+}
+*/
+
+	switch(check)
+	{
+		case 'a':
+			counta++;
+			break;
+		case 'b':
+			countb++;
+			break;
+		case 'c':
+			countc++;
+			break;
+		case 'd'
+	}
