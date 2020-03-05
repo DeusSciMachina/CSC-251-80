c7144381099b26ffc2b523a06da71f03462defbd
syang69 <syang69@csc208.csudh.edu>
2020-03-04 17:07:05 -0800

    first example

---
+++ example.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 30
+#define SIZE1 60
+
+void printer(int rname[SIZE]);
+void filleven(int rname[SIZE]);
+void fillodd(int rname[SIZE]);
+void sortoddandeven(int rname[], int rname2[]);
+
+int main(void)
+{
+	int arr[SIZE]= {0};
+	int arr1[SIZE]= {0};
+	int arr2[SIZE1]= {0};
+	printer (arr);
+	filleven(arr);
+	printer(arr);
+	fillodd(arr1);
+	printer(arr1);
+	sortoddandeven(arr, arr1);
+	printer(arr);
+	printer (arr1);
+	return EXIT_SUCCESS;
+}
+
+void printer(int rname[SIZE])
+{
+	int i =0;
+	for(i = 0; i <SIZE; i++)
+	{
+		printf("Array [%d]:	%d\n", i, rname[i]);
+	}
+		printf("\n");
+}
+
+void filleven(int rname[SIZE])
+{
+	int evenVal=0;
+	int i;
+	for (i = 0;i < SIZE; i++)
+	{
+		evenVal+=2;
+		rname[i] = evenVal;
+	}
+}	
+void fillodd(int rname[SIZE])
+{
+	int oddVal = 1;
+        int i;
+	for (i = 0; i < SIZE; i++)
+	{
+		oddVal +=2;
+		rname[i] = oddVal;
+	}	
+}
+void sortoddandeven(int rname[SIZE], int rname2[SIZE])
+{
+	int i;
+	for(i= 0; i < SIZE; i++)
+	{
+		int temp = rname[i];
+		int temp2 = rname2[i];
+		rname[i] = temp2;
+		rname2[i] = temp;
+	}
+}
+
